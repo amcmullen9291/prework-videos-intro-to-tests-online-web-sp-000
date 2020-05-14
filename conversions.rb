3b8d1712218c8b4863140a191af9ada32b4129d6
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-14 16:06:25 +0000

    Automatically backed up by Learn

--- conversions.rb
+++ conversions.rb
@@ -2,3 +2,8 @@ GRAMS_PER_OUNCE = 28.3495
 def ounces_to_grams(ounces)
   ounces.to_f * GRAMS_PER_OUNCE
 end
+
+def meal_choice(veg, starch, protein="meat")
+  meal = "A plate of #{protein} with #{veg} and #{starch}." puts "What a nutritious meal!" 
+  puts meal 
+end
